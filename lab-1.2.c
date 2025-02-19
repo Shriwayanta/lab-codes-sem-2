87de320c51841a6df33fb2842a76c92abde76d85
Shriwayanta <shri.rivu@gmail.com>
2025-02-19 20:24:41 +0530

    .

---
+++ lab-1.2.c
@@ -0,0 +1,28 @@
+//method two- largest element in an array using recursion
+
+#include <stdio.h>
+
+int findlargest(int arr[], int n) {
+    if (n == 1) {
+        return arr[0];
+    }
+    int max_of_rest = findlargest(arr, n - 1);
+    return (arr[n - 1] > max_of_rest) ? arr[n - 1] : max_of_rest;
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];  
+    printf("Enter %d elements in the array:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = findlargest(arr, n); 
+    printf("The largest element in the array is: %d\n", largest);
+
+    return 0;
+}
---
+++ lab-1.3.c
@@ -0,0 +1,29 @@
+//method three- largest element in an array using function
+
+#include <stdio.h>
+
+void funclargest(int arr[], int n){
+    int i, largest;
+
+    largest = arr[0];
+    for (i = 1; i < n; i++){
+        if (arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+    printf("Largest element is %d\n", largest);
+}
+int main(){
+    int n;
+    printf("Enter the number of elements : ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements :\n", n);
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+        }
+    funclargest(arr, n);
+    return 0;
+
+}
